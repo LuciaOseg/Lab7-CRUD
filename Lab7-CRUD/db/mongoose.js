34763caf0d50932eff93834597683d434e558043
Lucia Oseguera <48133614+LuciaOseg@users.noreply.github.com>
2019-11-19 14:56:45 -0600

    Update mongoose.js

--- Lab7-CRUD/db/mongoose.js
+++ Lab7-CRUD/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const connectionURL =`mongodb+srv://LuciaOseg:Clase12345@cluster0-azb6v.mongodb.net/test?retryWrites=true&w=majority`
+const connectionURL =`mongodb+srv://LuciaOseg:Maya2014.@cluster0-azb6v.mongodb.net/test?retryWrites=true&w=majority`
 
 mongoose.connect(connectionURL, {
   useNewUrlParser: true,
